9d5f595cb3c16f0fd739c1f8f817f31502af4495
Alex Y Suzuki <123485435+alexysbr@users.noreply.github.com>
2023-03-25 22:23:49 -0300

    quase pronto

--- Hello Mundo.py
+++ Hello Mundo.py
@@ -1,6 +1,20 @@
 from tkinter import *
 
 def hello():
-    print ('Hello Mundo') 
+    #print ('Hello Mundo') 
+    texto_hello["text"] = 'Hello Mundo'
 
-hello()
+janela = Tk()
+
+janela.title('Primeiro programa em py com interface gráfica Tkinter')
+
+texto_orientacao = Label(janela, text="Click no botão para mais detalhes do que esse pro")
+texto_orientacao.grid(column = 0,row = 0)
+
+botao = Button(janela, text='click', command=hello)
+botao.grid(column = 0, row = 1)
+
+texto_hello = Label(janela, text="")
+texto_hello.grid(column = 0, row = 2)
+
+janela.mainloop()
