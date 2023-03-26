db3f3c78b28d2671c95b840b7e72d64c86a436cf
Alex Y Suzuki <123485435+alexysbr@users.noreply.github.com>
2023-03-25 22:28:35 -0300

    Certo

--- Hello Mundo.py
+++ Hello Mundo.py
@@ -5,16 +5,17 @@ def hello():
     texto_hello["text"] = 'Hello Mundo'
 
 janela = Tk()
+#janela.geometry('400x400')
 
 janela.title('Primeiro programa em py com interface gráfica Tkinter')
 
-texto_orientacao = Label(janela, text="Click no botão para mais detalhes do que esse pro")
-texto_orientacao.grid(column = 0,row = 0)
+texto_orientacao = Label(janela, text="Click no botão para mais detalhes \ndo que esse programa faz")
+texto_orientacao.grid(column = 0,row = 0, padx = 10, pady = 10)
 
-botao = Button(janela, text='click', command=hello)
-botao.grid(column = 0, row = 1)
+botao = Button(janela, text='Click me', command=hello)
+botao.grid(column = 0, row = 1, padx = 10, pady = 10)
 
 texto_hello = Label(janela, text="")
-texto_hello.grid(column = 0, row = 2)
+texto_hello.grid(column = 0, row = 2, padx = 10, pady = 10)
 
 janela.mainloop()
