f5346a976039eacf42c9877767093a76ab90c637
Alex Y Suzuki <123485435+alexysbr@users.noreply.github.com>
2023-04-09 19:55:45 -0300

    botoes

--- vocabulario.py
+++ vocabulario.py
@@ -5,20 +5,39 @@ from time import sleep
 vocabulario = ('inu',  'neko', 'usagui', 'kame', 'namazu', 'baadii', 'mendori', 'niwatori', 'tori',)
 combinado = ('cão', 'gato', 'coelho', 'tartaruga', 'bagre', 'passarinho', 'galinha', 'frango', 'passaro')
 
+janela = Tk()
+janela.title('1º')
+
 cont = 0
-lista = (0,0,0,0,0,0,0,0)
+lista = [0,0,0,0,0,0,0,0]
 incompleto = True
-while incompleto:
-    
-    indice = randint(0,8)
+while incompleto:    
+    indice = randint(0,7)
     if lista[indice] == 0:
-        lista[indice] = 1
+        lista[indice] = 1        
+        botao = Button(janela, text = combinado[indice], command=())
+        botao.grid(column = 0, row = cont, padx = 10, pady = 10)
         cont += 1
+        #print(f'{lista}')
     if cont == 8:
         incompleto = False
+
+cont = 0
+lista = [0,0,0,0,0,0,0,0]
+incompleto = True
+while incompleto:    
+    indice = randint(0,7)
+    if lista[indice] == 0:
+        lista[indice] = 1        
+        botao = Button(janela, text = vocabulario[indice], command=())
+        botao.grid(column = 1, row = cont, padx = 10, pady = 10)
+        cont += 1
+        #print(f'{lista}')
+    if cont == 8:
+        incompleto = False
+janela.mainloop()        
         
-        
-    print(f'{lista}')
+    
     
 
 
