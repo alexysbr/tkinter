22c703dc77eecfc11c4aa033202bb056ff838810
Alex Y Suzuki <123485435+alexysbr@users.noreply.github.com>
2023-04-09 17:26:48 -0300

    programa para vocabulario

---
+++ vocabulario.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from random import randint
+from time import sleep
+
+computador = randint(0,2)
+vocabulario = ('inu', 'cão', 'neko', 'gato', 'usagui', 'coelho', 'kame', 'tartaruga', 'namazu', 'bagre',
+               'baadii', 'passarinho', 'mendori', 'galinha', 'niwatori', 'frango', 'tori', 'passaro')
+
